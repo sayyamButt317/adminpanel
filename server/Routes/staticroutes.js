c5386493361d195622285a3c2e6fe5b1c8c8850c
IT <sayyambutt317@gmail.com>
2024-02-18 00:04:30 +0500

    server update

---
+++ server/Routes/staticroutes.js
@@ -0,0 +1,6 @@
+import react from 'react';
+const UserModel = require('./Model/User')
+
+app.get('/create',(req, res) =>{
+For
+})
--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app =express()
-const UserModel = require('./Model/User')
-
+const Routes =require('./Routes/staticroutes')
 app.use(cors())
 app.use(express.json())
 
 mongoose.connect("mongodb://127.0.0.1:27017/AdminPanel")
 
+app.use("/",Routes)
 app.listen(3001,(req,res)=>{
     console .log("server started")
 })
